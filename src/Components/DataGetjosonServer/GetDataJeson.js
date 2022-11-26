f91ca8aa1a29a8eb173c0f4472131a5d9624e4ce
engektiar <eng.ektiar@gmail.com>
2022-11-26 21:52:06 +0600

    function add kora hois a

--- src/Components/DataGetjosonServer/GetDataJeson.js
+++ src/Components/DataGetjosonServer/GetDataJeson.js
@@ -2,19 +2,30 @@ import React, { useState, useEffect } from "react";
 
 const GetDataJeson = () => {
   const [user, setuser] = useState([]);
-  useEffect(() => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const getalluser = () => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         setuser(data);
-        console.log(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
       });
+  };
+  useEffect(() => {
+    getalluser();
   }, []);
   return (
     <div>
       <h1>Get data Fack api Thaka </h1>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
       {user &&
         user.map((item) => {
           const { id, title } = item;
